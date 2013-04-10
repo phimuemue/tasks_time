b7670a788490e58112cb40f4438c605d6a6c6e5a
Philipp Mueller <muellerp@atsccs56.informatik.tu-muenchen.de>
2013-04-10 12:12:08 +0200

    Brought task to current state from prototype

--- src/task.cpp
+++ src/task.cpp
@@ -1,5 +1,10 @@
 #include "task.h"
 
+Task::Task(int id, Distribution d){
+    this->id = id;
+    distribution = d;
+}
+
 void Task::elapse(myfloat t){
     elapsed += t;
 }
@@ -12,7 +17,28 @@ myfloat Task::get_elapsed(){
     return elapsed;
 }
 
-myfloat get_expected_remaining_time(){
-    // TODO: to be implemented
+myfloat Task::get_expected_remaining_time(){
+    switch(distribution){
+        case(Exponential):
+            // Exponential distribution is memoryless
+            return exponential_lambda;
+        case(Uniform):
+            // Uniform distribution not memoryless
+            if(elapsed > uniform_b){        
+                throw 0;
+            }
+            return (uniform_b - elapsed) / 2;
+    }
     throw 0;
+}
+
+bool Task::operator==(const Task& t){
+    return t.id == id;
+}
+
+ostream& operator<<(ostream& os, const Task& t){
+    os << t.id;
+    if(t.elapsed > 0)
+        os << "(" << t.elapsed << ")";
+    return os;
 }
--- src/task.h
+++ src/task.h
@@ -1,7 +1,11 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include<iostream>
+
 #include "config.h"
+
+using namespace std;
 
 /*  A task represents a task to be done, and holds additional information
     so that it can act as a random variable (thus also representing the
@@ -9,6 +13,7 @@
 */
 class Task {
 private:
+    int id;
     myfloat elapsed;
     Distribution distribution;
     union {
@@ -21,16 +26,19 @@ private:
             myfloat uniform_a;
             myfloat uniform_b;
         };
-        // stuff for poisson distribution
-        struct {
-            myfloat poisson_lambda;
-        };
     };
 public:
+    Task(int id, Distribution d);
+
     void elapse(myfloat t);
     void set_elapsed(myfloat t);
     myfloat get_elapsed();
+
     myfloat get_expected_remaining_time();
+
+    bool operator==(const Task& t);
+
+    friend ostream& operator<<(ostream& os, const Task& t);
 };
 
 #endif
