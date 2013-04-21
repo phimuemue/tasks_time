a2f034d74c77ed9c3109d8820fe3ca526b433d33
Philipp Müller <descpl@yahoo.de>
2013-04-21 11:25:48 +0200

    Version bump to 0.1

--- src/info.cpp
+++ src/info.cpp
@@ -2,9 +2,9 @@
 
 char builtby[] = "philipp";
 char version[] = "0";
-char minor[] = "0";
-char build[] = "705";
-char date[] = "2013/04/21 11:24:44";
+char minor[] = "1";
+char build[] = "1";
+char date[] = "2013/04/21 11:25:31";
 
 using namespace std;
 
