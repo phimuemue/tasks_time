6174d1513c02fa776d1f6f7f13c6cc329ec3ef2e
Philipp Mueller <muellerp@atsccs33.informatik.tu-muenchen.de>
2013-06-05 10:57:21 +0200

    Only print short version information

--- src/info.cpp
+++ src/info.cpp
@@ -3,14 +3,15 @@
 char builtby[] = "muellerp";
 char version[] = "0";
 char minor[] = "3";
-char build[] = "125";
-char date[] = "2013/06/05 10:54:56";
+char build[] = "127";
+char date[] = "2013/06/05 10:56:59";
 
 using namespace std;
 
 void print_version(bool long_version){
-    cout << "-----------------------------------------------------------------" << endl;
-    cout << "+  " << endl << 
+    if(long_version){
+        cout << "-----------------------------------------------------------------" << endl;
+        cout << "+  " << endl << 
             "+         o---o---o---o---o-\\           " << endl <<
             "+                            \\          tasks_time" << endl <<
             "+   o---o---o-\\     o-\\       \\         " << version << "." << minor << "." << build << endl <<
@@ -19,11 +20,19 @@ void print_version(bool long_version){
             "+              /           /            " << builtby << endl <<
             "+   o---o---o-/     o---o-/             " << endl << 
             "+  " << endl;
-    cout << "-----------------------------------------------------------------" << endl;
-    cout << "+   tasks_time " << version << "." << minor
-         << "." << build
-         << " built " << date
-         << " by " << builtby 
-         << endl;
-    cout << "-----------------------------------------------------------------" << endl << endl;
+        cout << "-----------------------------------------------------------------" << endl;
+        cout << "+   tasks_time " << version << "." << minor
+            << "." << build
+            << " built " << date
+            << " by " << builtby 
+            << endl;
+        cout << "-----------------------------------------------------------------" << endl << endl;
+    }
+    else {
+        cout << "tasks_time " << version << "." << minor
+            << "." << build
+            << "(" << date
+            << ", " << builtby << ")"
+            << endl;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -132,7 +132,7 @@ int read_variables_map_from_args(int argc,
     try{
         po::store(po::parse_command_line(argc, argv, desc), vm);
         if(vm.count("help")){
-            print_version(false);
+            print_version(true);
             cout << "Help." << endl
                 << desc << endl;
             return 1;
@@ -249,7 +249,7 @@ int main(int argc, char** argv){
     omp_set_nested(1);
 #endif
 
-    print_version(true);
+    print_version(false);
 
     // vector<pair<Task, Task>> e1;
     // tree_from_string("0 0 0 1 1 2 2 3", e1);
