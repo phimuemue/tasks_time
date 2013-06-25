036d3bac7bf42d49213049371e752dba2e3d9e40
Philipp Mueller <muellerp@atsccs59.informatik.tu-muenchen.de>
2013-06-25 15:01:21 +0200

    Minor change in chainsideexporter

--- src/chainsideexporter.cpp
+++ src/chainsideexporter.cpp
@@ -6,7 +6,7 @@ void ChainSideExporter::export_single_snapshot_internal(ostream& output,
         const map<task_id, vector<task_id>>& rt,
         const unsigned int depth,
         const float leftoffset) const {
-    output << "\\node[circle, scale=0.75] at (0, 0) {";
+    output << "\\node[rectangle, scale=0.75] at (0, 0) {";
     output << "$" <<
         s.intree.longest_chain_length() <<
         "/" <<
