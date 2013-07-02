6046651689e4c65618ae902c2fe74ce6a26561f9
Philipp Mueller <muellerp@atsccs59.informatik.tu-muenchen.de>
2013-07-02 15:40:27 +0200

    Dynamic naming of target

--- src/info.cpp
+++ src/info.cpp
@@ -3,8 +3,8 @@
 char builtby[] = "muellerp";
 char version[] = "0";
 char minor[] = "4";
-char build[] = "77";
-char date[] = "2013/07/02 15:31:29";
+char build[] = "81";
+char date[] = "2013/07/02 15:39:33";
 
 using namespace std;
 
