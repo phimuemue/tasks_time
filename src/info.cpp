8caf66e2085299b4186ea46eb85f580d1ddc1c1e
Philipp Müller <descpl@yahoo.de>
2013-08-28 16:50:09 +0200

    Filename underscore improvement

--- src/info.cpp
+++ src/info.cpp
@@ -3,8 +3,8 @@
 char builtby[] = "philipp";
 char version[] = "0";
 char minor[] = "4";
-char build[] = "486";
-char date[] = "2013/08/27 18:20:40";
+char build[] = "519";
+char date[] = "2013/08/28 16:49:16";
 
 using namespace std;
 
--- src/tikztopdf.cpp
+++ src/tikztopdf.cpp
@@ -21,7 +21,8 @@ void TikzToPdf::convertTikzToPdf(string infile, string outfile){
         R"(\input{)" + infile + R"(})"
         R"(\end{document})"
         ;
-    string command = "pdflatex -interaction=batchmode -jobname=" + outfile + " \"" + tex_code + "\"";
+    string command = "pdflatex -interaction=batchmode -jobname='" + outfile + "' \"" + tex_code + "\"";
+    cout << command << endl;
     FILE* pdflatex = popen(command.c_str(), "r");
     pclose(pdflatex);
     FILE* pdfcrop = popen(("pdfcrop " + outfile + ".pdf " + outfile + ".pdf").c_str(), "r");
