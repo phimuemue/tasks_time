5dba685a12cf8362f8a3650724c4996bdc1c25e2
Philipp Müller <descpl@yahoo.de>
2013-10-13 11:11:53 +0200

    Rudimentary tree output on command line

--- src/info.cpp
+++ src/info.cpp
@@ -3,8 +3,8 @@
 char builtby[] = "philipp";
 char version[] = "0";
 char minor[] = "4";
-char build[] = "1013";
-char date[] = "2013/10/13 10:49:07";
+char build[] = "1015";
+char date[] = "2013/10/13 11:11:11";
 
 using namespace std;
 
